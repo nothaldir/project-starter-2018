b5165161407a8e73e48f01f14a6f6b3ea0a3cf78
Alex Berneau <alex@moworks.com.au>
2018-08-08 16:56:45 +1000

    refactor(views): html/pug to php

--- app/views/index.php
+++ app/views/index.php
@@ -6,11 +6,15 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
   <link rel="stylesheet" href="assets/index.css">
+  <link rel="stylesheet" href="assets/icons/_sprite.css">
 </head>
 <body>
-  <div class="hello">
-    hello
+  <div class="helloworld">
+    hello world
   </div>
+  <svg class="svg-facebook-dims">
+    <use xlink:href="assets/icons/svg/sprite.css.svg#facebook"></use>
+  </svg>
   <script src="assets/bundle.js" type="module"></script>
 </body>
 </html>
