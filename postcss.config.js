cede39862931acbcf29f6b0edfb804de4a2b9719
Alex Berneau <alex@moworks.com.au>
2018-08-20 19:03:12 +1000

    :wrench: (postcss) add import plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,18 @@
 module.exports = {
   parser: 'postcss-scss',
   plugins: [
+    require('postcss-easy-import')({
+      prefix: '_',
+      extensions: [
+        '.scss',
+        '.css'
+      ]
+    }),
     require('@csstools/postcss-sass')({
       outputStyle: 'compressed'
     }),
     require('autoprefixer')({
       grid: true
-    }),
+    })
   ],
 };
